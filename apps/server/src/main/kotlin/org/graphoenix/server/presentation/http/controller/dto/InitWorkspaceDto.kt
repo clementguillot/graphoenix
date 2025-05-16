af16c24983f6379736652d6dbac29c23d71e9114
Clément Guillot <guillot.clement@pm.me>
2025-05-16 15:24:58 +0200

    fix(server): missing registered for reflection DTO

--- apps/server/src/main/kotlin/org/graphoenix/server/presentation/http/controller/dto/InitWorkspaceDto.kt
+++ apps/server/src/main/kotlin/org/graphoenix/server/presentation/http/controller/dto/InitWorkspaceDto.kt
@@ -1,5 +1,8 @@
 package org.graphoenix.server.presentation.http.controller.dto
 
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@RegisterForReflection
 data class InitWorkspaceDto(
   val url: String,
   val token: String,
